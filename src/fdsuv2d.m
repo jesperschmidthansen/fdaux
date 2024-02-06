cbfc2c3ef39716cdad8e631452e7a550486826b3
Jesper Schmidt Hansen <jschmidt@nb-dk1081102.ruc.dk>
2024-02-06 20:59:04 +0100

    Added help txt to some functions

--- src/fdsuv2d.m
+++ src/fdsuv2d.m
@@ -1,11 +1,31 @@
 #
-# Calculate the stream function and velocity fields from the vorticity
+# Usage: [psi, u, v] = fdsuv2d(w, bcs, sorfac, error)
+#
+# Calculate the stream function and velocity field from the vorticity.  
+#
+# input
+# w: The vorticity field
+# bcs: Boundary conditions. Valid values are 'p' (periodic) or 'd' (zero Dirichelt). Default is 'p'
+# sorfac: Factor for the SOR algorithm. Value between 1 and 2 - default is 1.5. Ignored for bcs='p'
+# error: Maximum allowed error for the SOR algorithm. Default 1e-5. Ignored for bcs='p'   
+#
+# output
+# pst: Stream function
+# u: Velocity x-component
+# v: velocity y-component
+#
+# example
+# [psi, u, v] = fdsuv2d(w, 'd', 1.75, 1.0e-6);
 #
 function [psi, u, v] = fdsuv2d(w, bc='p', sorfac=1.5, _error=1.0e-5)
 		
 	ngrdx = w.ngrdx; ngrdy = w.ngrdy;
 	dx = w.dx; dy = w.dy;
    
+	if nargin > 4 
+		error("Number of input arguments cannot exceed 4");
+	end
+	
 	if bc=='p'
 		bcs = "pppp";
 	elseif bc=='d';
--- src/fdvteq2d.m
+++ src/fdvteq2d.m
@@ -1,16 +1,29 @@
 
-
+#
 # Usage dwdt = fdveq2d(time now, cell array)
 #
-# Right-hand side of the two dimensional vortecity equation 
+# Right-hand side of the two dimensional vorticity equation 
 #
 # input 
 # time now: the current time
-# cell array: A cell array with element {vorticity field, viscosity, 
-# 
+# cell array: A cell array with five elements 
+#            {vorticity field, stream function, u field, v field, viscosity} 
+#            
+# output
+# dwdt: Derivative of vorticity field           
 #
-function [dwdt psi u v] = fdvteq2d(time, funvar)
+# exampe
+# dwdt = fdvteq2d(time, {w, psi, u, v, 1.0});
+#
+function dwdt  = fdvteq2d(time, funvar)
 
+	if nargin!=2
+		error("Number of inputs must be 2"); 	
+	end
+	if length(funvar)!=5
+		error("Length of cell array input argument must be 5");
+	end	
+	
 	w = funvar{1}; psi = funvar{2};
 	u=funvar{3}; v=funvar{4}; 
 	visc = funvar{5};
