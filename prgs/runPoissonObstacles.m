a069e6ed23d1e1dd3df4ccd8419786a42b169ac7
Jesper Schmidt Hansen <jschmidt@nb-dk1081102.ruc.dk>
2024-09-23 21:49:11 +0200

    Added an example of Poisson problem with constraints/obstacles

---
+++ prgs/runPoissonObstacles.m
@@ -0,0 +1,23 @@
+# 
+# Poisson problem with obstavles
+#
+
+clear
+
+lx  = 5; ly  = lx;
+ngx = 100; ngy = ngx;
+dx = lx/ngx; dy = ly/ngy;
+
+u = zeros(ngy, ngx); 
+force = 0.05*ones(ngy, ngx);
+
+c_idx = zeros(ngx, ngx); c_idx(45:55, 45:55)=1; c_idx(20:30, 20:30)=1;
+c_val = zeros(ngx, ngx);
+
+for n=1:10
+	[u, niter, status] = ofdsor(u, -force, [dx dy], 1.5, 1e-8, c_idx, c_val);
+	printf("%d\n", niter);
+end
+
+surf(u)
+
